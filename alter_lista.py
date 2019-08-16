94e9f8f1c6c6fe258b61a6b61be6e8aca4bc1b13
Jessica Meira <jessica.meira1@gmail.com>
2019-08-15 21:01:36 -0300

    Update alter_lista.py
    trabalhando com listas, remove, append, del, insert, pop.

--- alter_lista.py
+++ alter_lista.py
@@ -11,3 +11,28 @@ print(motos)
 #O Metodo insert adiciona no local informado alterando a posição dos outros itens da lista
 motos.insert(2, 'BMW')
 print(motos)
+
+#del pode deletar valores de uma lista
+del motos[5]
+print(motos)
+
+#a instrução pop deleta o ultimo registro da lista,
+#nesse caso passamos os registro deletado para outra variavel.
+popped_motos = motos.pop()
+print (motos)
+print(popped_motos)
+
+#passando o indice para o pop
+popped_motos2 = motos.pop(0)
+print ("A primeira moto que eu comprei foi a " + popped_motos2.title())
+
+#remove pode ser usado para remover o valor que seja igual ao que foi passado
+motos.remove('kawasaki')
+print(motos)
+
+muito_caro = 'BMW'
+motos.remove(muito_caro)
+print("Removi a " + muito_caro.title() + " Pois é muito caro pra mim" )
+# O Metodo remove, apaga apenas o primeiro registro que contenha a 
+#informação que encontrar, caso haja outros é necessário utilizar uma
+#estrutura de repetição.
